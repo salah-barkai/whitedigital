c75bcc3d6a9d4f5f5d4a82e919d3213424965f9b
saladine ahmat barkai <93881972+salah-barkai@users.noreply.github.com>
2024-01-24 18:52:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+ function toggleNav() {
+        var nav = document.querySelector('nav');
+        nav.classList.toggle('show');
+    }
+
+function gotocontact(){
+	var btn_contact = document.querySelector('.contact');
+
+	btn_contact.addEventListener("click",() => {
+       window.location.href = 'contact.html';
+	})
+};
+
+function gotohome(){
+	var btn_home = document.querySelector('.home');
+
+	btn_home.addEventListener("click",() => {
+       window.location.href= 'navbarstyle.html';
+	})
+};
+
+function gotosvg(){
+	var btn_svg = document.querySelector('.svg');
+
+	btn_svg.addEventListener("click",() => {
+       window.location.href= 'svg.html';
+	})
+};
+
+
+gotosvg();
+gotohome();
+gotocontact();
